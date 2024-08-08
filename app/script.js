1331652c4cc7d2ab00c79300834a1ecabbe43b60
PrinceGuruCode237 <princefall2111@gmail.com>
2024-08-08 21:34:58 +0100

    Now the user can not only see the game page starter but only when it hits the space keyboard he sees the header which shows the score, speed, level and buttons to control the game

--- app/script.js
+++ app/script.js
@@ -2,13 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
   // Retrieve all the variables for the DOM manipulation
   const gameAreaEl = document.querySelector(".game-area");
   const startGameEl = document.querySelector(".start");
+  const body = document.body;
 
-  /////////// ---------- Listening the space keyboard event --- ////////////
+  /////////// ---------- Listening the space keyboard event to start the game --- ////////////
   document.addEventListener("keypress", (e) => {
     const spaceKeyBoard = e.key;
     if (spaceKeyBoard === " ") {
       startGameEl.classList.add("hide");
       gameAreaEl.classList.add("show");
+      body.classList.add("levelOne");
     }
   });
 });
