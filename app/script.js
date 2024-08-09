e8299ec8a1377abca71c5725fe380d84d4f4f9b1
PrinceGuruCode237 <princefall2111@gmail.com>
2024-08-09 10:45:45 +0100

    Adding the the menu bar so now when the user clicks on the hamburger it shows the menu, he can also close wether by clicking on the close button or on the backdrop

--- app/script.js
+++ app/script.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const gameAreaEl = document.querySelector(".game-area");
   const startGameEl = document.querySelector(".start");
   const body = document.body;
+  const options = document.querySelectorAll(".option");
+  const hamburgerMenuEl = document.querySelector(".hamburger");
+  const menuBarEl = document.querySelector(".menu-bar");
+  const closeMenuEl = document.getElementById("close");
 
   /////////// ---------- Listening the space keyboard event to start the game --- ////////////
   document.addEventListener("keypress", (e) => {
@@ -11,6 +15,27 @@ document.addEventListener("DOMContentLoaded", () => {
       startGameEl.classList.add("hide");
       gameAreaEl.classList.add("show");
       body.classList.add("levelOne");
+    }
+  });
+  // Adding a tick on the suboptions (uppercase, lowercase, numbers and symbols)
+  options.forEach((option) => {
+    option.addEventListener("click", () => option.classList.toggle("checked"));
+  });
+
+  console.log(menuBarEl);
+  hamburgerMenuEl.addEventListener("click", () => {
+    menuBarEl.classList.add("show");
+  });
+
+  // Toggle the menubar on the click of the close Button
+  closeMenuEl.addEventListener("click", () =>
+    menuBarEl.classList.remove("show")
+  );
+
+  // Toggle the menubar on the click of the backdrop
+  body.addEventListener("click", (e) => {
+    if (e.target.classList.contains("menu-bar")) {
+      menuBarEl.classList.remove("show");
     }
   });
 });
